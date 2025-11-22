29c69fce1eae1c970a10e56abd248db7c7d34a05
rappelt <rc.appelt@gmail.com>
2025-11-22 16:54:44 +0100

    scroll to top eingefügt

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Index from "./pages/Index";
 import Manufacturers from "./pages/Manufacturers";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
+
 
 const queryClient = new QueryClient();
 
@@ -18,6 +20,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+        <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/manufacturers" element={<Manufacturers />} />
